b1077c59d20f3994d2f35a0799f4bcc8bc7ffaf5
infinite-kemonoyagi <cakefelino20@gmail.com>
2025-11-23 13:49:20 -0700

    add basic fields

--- source/funkin/visual/text/FunkinTextCharacter.hx
+++ source/funkin/visual/text/FunkinTextCharacter.hx
@@ -4,6 +4,16 @@ import funkin.backend.MusicBeatSprite;
 
 class FunkinTextCharacter extends MusicBeatSprite
 {
+    public var parent:Null<FunkinText> = null;
+
+    public var character:String;
+    public var font:String;
+    public var size:Int;
+    public var row:Int;
+    public var lastCharacter(default, null):Null<FunkinTextCharacter> = null;
+    public var lastSpaces(default, null):Float = 0.0;
+    public var lastRows(default, null):Float = 0.0;
+
     public function new()
     {
         super();
