0414402a83b88674296d924739e05ccc6ee5b0d2
infinite-kemonoyagi <cakefelino20@gmail.com>
2025-11-23 14:10:09 -0700

    incorporate assets field in Main

--- source/Main.hx
+++ source/Main.hx
@@ -3,16 +3,21 @@ package;
 import flixel.FlxG;
 import flixel.FlxGame;
 import flixel.FlxSprite;
+import funkin.assets.FunkinAssets;
 import funkin.menu.MenuState;
 import openfl.display.Sprite;
 
 class Main extends Sprite
 {
+	public static var assets(default, null):FunkinAssets;
+
 	public function new()
 	{
 		super();
 		
 		addChild(new FlxGame());
+
+		assets = new FunkinAssets();
 
 		FlxG.fixedTimestep = false;
 		FlxSprite.defaultAntialiasing = true;
